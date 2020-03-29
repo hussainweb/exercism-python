ffd897e4551dcae2593e3bf8d8807f62053c85bc
hw <hussainweb@gmail.com>
2020-03-29 01:34:39 -0400

    Implement solution for matrix

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,9 +1,15 @@
 class Matrix:
+    matrix = []
+
     def __init__(self, matrix_string):
+        rows = matrix_string.split("\n")
+        self.matrix = [[int(col) for col in row.split(" ")] for row in rows]
         pass
 
     def row(self, index):
+        return self.matrix[index-1]
         pass
 
     def column(self, index):
+        return [row[index-1] for row in self.matrix]
         pass
