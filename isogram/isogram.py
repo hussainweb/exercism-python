504cdedca5bb7e2d9d5fcd8a9e795287ecee0775
hw <hussainweb@gmail.com>
2020-04-05 18:23:57 -0400

    Implemented solution for isogram.

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,7 @@
 def is_isogram(string):
-    pass
+    chars = {}
+    for c in string.lower():
+        if (c.isalpha() and c in chars.keys()):
+            return False
+        chars[c] = True
+    return True
