6cf5decb45fdd3a90c78de1dc093f2ae4d848a54
hw <hussainweb@gmail.com>
2020-05-04 23:58:09 -0400

    Moved dictionaries to global/module scope.

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -1,45 +1,44 @@
-def getVerses():
-    return {
-        1: "a Partridge in a Pear Tree",
-        2: "two Turtle Doves",
-        3: "three French Hens",
-        4: "four Calling Birds",
-        5: "five Gold Rings",
-        6: "six Geese-a-Laying",
-        7: "seven Swans-a-Swimming",
-        8: "eight Maids-a-Milking",
-        9: "nine Ladies Dancing",
-        10: "ten Lords-a-Leaping",
-        11: "eleven Pipers Piping",
-        12: "twelve Drummers Drumming",
-    }
+allVerses = {
+    1: "a Partridge in a Pear Tree",
+    2: "two Turtle Doves",
+    3: "three French Hens",
+    4: "four Calling Birds",
+    5: "five Gold Rings",
+    6: "six Geese-a-Laying",
+    7: "seven Swans-a-Swimming",
+    8: "eight Maids-a-Milking",
+    9: "nine Ladies Dancing",
+    10: "ten Lords-a-Leaping",
+    11: "eleven Pipers Piping",
+    12: "twelve Drummers Drumming",
+}
+
+numbersToWord = {
+    1: "first",
+    2: "second",
+    3: "third",
+    4: "fourth",
+    5: "fifth",
+    6: "sixth",
+    7: "seventh",
+    8: "eighth",
+    9: "ninth",
+    10: "tenth",
+    11: "eleventh",
+    12: "twelfth",
+}
 
 
 def getFirstLine(number):
-    numbers = {
-        1: "first",
-        2: "second",
-        3: "third",
-        4: "fourth",
-        5: "fifth",
-        6: "sixth",
-        7: "seventh",
-        8: "eighth",
-        9: "ninth",
-        10: "tenth",
-        11: "eleventh",
-        12: "twelfth",
-    }
-    return f'On the {numbers[number]} day of Christmas my true love gave to me: '
+    return f"On the {numbersToWord[number]} day of Christmas my true love gave to me: "
 
 
 def getVerse(number):
     verse = getFirstLine(number)
-    lines = getVerses()
     for line in range(number, 0, -1):
-        if (line == 1 and line != number):
+        if line == 1 and line != number:
             verse += "and "
-        verse += lines[line]
+        verse += allVerses[line]
         verse += "." if line == 1 else ", "
     return verse
 
