400bb56dbac73646e34862066ca8b65ad570a374
hw <hussainweb@gmail.com>
2023-04-30 10:23:58 +0300

    remove unnecessary 'elif'

--- meltdown-mitigation/conditionals.py
+++ meltdown-mitigation/conditionals.py
@@ -40,12 +40,11 @@ def reactor_efficiency(voltage, current, theoretical_max_power):
     efficiency = voltage * current / theoretical_max_power * 100
     if efficiency >= 80:
         return "green"
-    elif efficiency >= 60:
+    if efficiency >= 60:
         return "orange"
-    elif efficiency >= 30:
+    if efficiency >= 30:
         return "red"
-    else:
-        return "black"
+    return "black"
 
 
 def fail_safe(temperature, neutrons_produced_per_second, threshold):
